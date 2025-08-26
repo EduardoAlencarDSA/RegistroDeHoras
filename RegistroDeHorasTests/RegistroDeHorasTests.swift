52767e1dc67e2d911c59e540b9d0672923123bd4
Eduardo Alencar <j.eduardoalencarq@gmail.com>
2025-08-26 23:02:55 +0000

    Initial Commit

---
+++ RegistroDeHorasTests/RegistroDeHorasTests.swift
@@ -0,0 +1,16 @@
+//
+//  RegistroDeHorasTests.swift
+//  RegistroDeHorasTests
+//
+//  Created by Eduardo Alencar on 26/08/2025.
+//
+
+import Testing
+
+struct RegistroDeHorasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
